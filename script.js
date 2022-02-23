4c6efe71d9c00f7ed9c267071f5429301a228cfd
Guilherme Cardoso <guilhermecardosowork@gmail.com>
2022-02-22 21:02:29 -0300

    parte 3

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 const hamburger = document.querySelector('.hamburger')
 const navLink = document.querySelector('.menu')
-const x = document.querySelector('.lineBack')
-const line1 = document.querySelector('#line1')
-const line2 = document.querySelector('#line2')
-const line3 = document.querySelector('#line3')
+
 
 hamburger.addEventListener('click', () => {
   navLink.classList.toggle('hide')
+  hamburger.classList.toggle('disable')
 })
+
